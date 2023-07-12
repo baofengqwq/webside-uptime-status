e67df667de2717f35cbcd7318fc31c64c9bd3c04
baofeng <baofengyo@outlook.com>
2023-07-12 18:43:40 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,41 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Baofeng Webside Status',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'u2215312-94b7081e6dda8453d539225c',
+  ],
+
+  // 日志天数
+  CountDays: 90,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: 'Home',
+      url: 'https://bao-feng.top'
+    },
+    {
+      text: 'Blog',
+      url: 'https://blog.bao-feng.top'
+    },
+    {
+      text: 'Data Blog',
+      url: 'https://data.bao-feng.top'
+    },
+    {
+      text: 'WebDisk',
+      url: 'http://47.113.223.213:5212'
+    },
+    {
+      text: 'Baofeng BingAI',
+      url: 'https://ai.bao-feng.top'
+    },
+  ],
+};
